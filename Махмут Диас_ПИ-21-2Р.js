c0cebec84a8ded3703343b29e217c8d61d5fe988
AWeskerA <129740002+AWeskerA@users.noreply.github.com>
2023-10-01 23:22:43 +0600

    Delete Махмут Диас_ПИ-21-2Р.js

--- Махмут Диас_ПИ-21-2Р.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const input = document.querySelector('.line');
-    input.addEventListener('input', function() {
-        const inputValue = this.value.toLowerCase().trim();
-        if (inputValue === 'смешно') {
-            alert('Молодец, Индус');
-        }
-    });
-});
