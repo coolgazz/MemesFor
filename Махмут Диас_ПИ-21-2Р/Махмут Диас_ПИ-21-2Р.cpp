8825b5b2577db3d8aa153ed372827d1848df1b0c
AWeskerA <129740002+AWeskerA@users.noreply.github.com>
2023-10-22 17:09:00 +0600

    Add files via upload
    Любой проект

---
+++ Махмут Диас_ПИ-21-2Р/Махмут Диас_ПИ-21-2Р.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <random>
+#include <time.h>
+int attempts = 0;
+
+int guessNumbers() {
+	return rand() % 100 + 1;
+}
+
+int main() {
+	srand(time(NULL));
+	setlocale(LC_ALL, "RUS");
+	
+	std::cout << "Добро пожаловать в игру 'Угадай число'!" << std::endl;
+	std::cout << "Я загадал число от 1 до 100. Попробуй угадать его." << std::endl;
+
+	int secretNumber = guessNumbers();
+
+	while (true) {
+		int userGuess;
+		std::cout << "Твоя попытка: ";
+		std::cin >> userGuess;
+		attempts += 1;
+
+		if (userGuess < secretNumber) {
+			std::cout << "Слишком маленькое число. Попробуй еще раз." << std::endl;
+		}
+		else if (userGuess > secretNumber) {
+			std::cout << "Слишком большое число. Попробуй еще раз" << std::endl;
+		}
+		else {
+			std::cout << "Поздравляю! Ты угадал число за " << attempts << " попыток." << std::endl;
+			break;
+		}
+	}
+	
+	return 0;
+	
+}
